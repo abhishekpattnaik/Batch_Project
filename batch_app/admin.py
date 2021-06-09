b470a5c5094b1f0c0b19f238af157fd47b512d10
abhishekpattnaik <abhishek.p@adcuratio.com>
2021-06-10 01:02:04 +0530

    added model

--- batch_app/admin.py
+++ batch_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from batch_app.models import Batch, Code
 # Register your models here.
+
+admin.site.register(Batch)
+admin.site.register(Code)
--- batch_app/models.py
+++ batch_app/models.py
@@ -1,3 +1,23 @@
+import string, random
 from django.db import models
+from django.contrib.auth.models import User
+from constants.constant import ALPHANUMERICLEN
 
-# Create your models here.
+class Batch(models.Model):
+    name = models.CharField(max_length=254, blank=True, default="")
+    batch_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Code(models.Model):
+    batch = models.ForeignKey("batch_app.Batch", on_delete=models.CASCADE, null=True, blank=True)
+    batch_code = models.CharField(max_length=ALPHANUMERICLEN, blank=False)
+
+    def save(self, *args, **kwargs):
+        letters = string.ascii_lowercase
+        self.batch_code = ''.join(random.choice(letters) for i in range(ALPHANUMERICLEN))
+        super(Code, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.batch_code
